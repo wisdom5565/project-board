b8686dd84149f6fd5b02834cf6ffed16588422c6
wisdom5565 <wisdom5565@naver.com>
2022-12-28 09:34:23 +0900

    #17 - 도메인 클래스 설계
    게시글, 댓글 설계

--- project-board/src/main/java/com/koreait/projectboard/domain/Article.java
+++ project-board/src/main/java/com/koreait/projectboard/domain/Article.java
@@ -1,4 +1,16 @@
 package com.koreait.projectboard.domain;
 
+import java.time.LocalDateTime;
+
 public class Article {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String hashtag;
+    private String createdBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
 }
--- project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
+++ project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
@@ -1,4 +1,13 @@
 package com.koreait.projectboard.domain;
 
+import java.time.LocalDateTime;
+
 public class ArticleComment {
+    private Long id;
+    private Article article;     // 게시글 아이디
+    private String content;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
 }
